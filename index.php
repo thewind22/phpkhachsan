40f99847e2609c54e99e0b32fc1c6bcd8c492f53
thewind22 <118001683+thewind22@users.noreply.github.com>
2024-04-11 08:45:04 +0000

    :pw

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 include 'controllers/RoomController.php';
 include 'controllers/UserController.php';
+include 'controllers/HomeController.php';
 
-
+$homeController = new HomeController();
 
 $userController = new UserController();
 $roomController = new RoomController();
@@ -20,9 +21,9 @@ if (!isset($_SESSION['user'])) {
     }
 }
 
-$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+$action = isset($_GET['action']) ? $_GET['action'] : 'getAllUsers';
 $roomId = isset($_GET['RoomID']) ? $_GET['RoomID'] : null;
-$userId = isset($_GET['UserID']) ? $_GET['UserID'] : null;
+$userId = isset($_GET['UserID']) ? $_GET['UserID'] : null;  
 
 
 
@@ -62,6 +63,9 @@ switch ($action) {
     case 'register':
           
             $userController->register();
+            break;
+    case 'home':
+            $homeController->home();
             break;
     case 'logout':
         // Nếu hành động là 'logout', kết thúc phiên đăng nhập và chuyển hướng đến trang đăng nhập
